1c344d7a09b7eec9a0f9938fea799d25dbe7b26e
Professor Breaker <jbreak1@ju.edu>
2025-11-04 15:15:07 -0500

    Adding camera button

--- LootLoggerRB/DetailViewController.swift
+++ LootLoggerRB/DetailViewController.swift
@@ -12,6 +12,9 @@ class DetailViewController: UIViewController, UITextFieldDelegate {
         view.endEditing(true)
     }
     
+    @IBAction func choosePhotoSource(_ sender: UIBarButtonItem) {
+    }
+    
     @IBOutlet var nameField: UITextField!
     @IBOutlet var serialNumberField: UITextField!
     @IBOutlet var valueField: UITextField!
